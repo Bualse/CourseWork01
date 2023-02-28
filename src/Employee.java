fb19bad7dd5f7f2bfa8be7e1c68f45612ee8a63d
budko <budkovalexandr@gmail.com>
2023-02-28 16:49:00 +0300

    1 commit

--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,99 @@
 public class Employee {
 
+    private String fullName;
+    private String departmentEmployee1;
+    private String departmentEmployee2;
+    private String departmentEmployee3;
+    private String departmentEmployee4;
+    private String departmentEmployee5;
+    private int salaryEmployee;
+    static int Counter;
+    private int id;
+
+    public Employee(int id, String fullName, String departmentEmployee, int salaryEmployee) {
+        this.fullName = fullName;
+        this.departmentEmployee1 = departmentEmployee;
+        this.departmentEmployee2 = departmentEmployee;
+        this.departmentEmployee3 = departmentEmployee;
+        this.departmentEmployee4 = departmentEmployee;
+        this.departmentEmployee5 = departmentEmployee;
+        this.salaryEmployee = salaryEmployee;
+        this.id = ++Counter;
+    }
+
+    public String getFullName() {
+
+        return fullName;
+    }
+
+    public String getDepartmentEmployee1() {
+
+        return departmentEmployee1;
+    }
+
+    public String getDepartmentEmployee2() {
+
+        return departmentEmployee2;
+    }
+
+    public String getDepartmentEmployee3() {
+
+        return departmentEmployee3;
+    }
+
+    public String getDepartmentEmployee4() {
+
+        return departmentEmployee4;
+    }
+
+    public String getDepartmentEmployee5() {
+
+        return departmentEmployee5;
+    }
+
+    public int getSalaryEmployee() {
+
+        return salaryEmployee;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setDepartmentEmployee1(String departmentEmployee1) {
+
+        this.departmentEmployee1 = departmentEmployee1;
+    }
+
+    public void setDepartmentEmployee2(String departmentEmployee2) {
+
+        this.departmentEmployee2 = departmentEmployee2;
+    }
+
+    public void setDepartmentEmployee3(String departmentEmployee3) {
+
+        this.departmentEmployee3 = departmentEmployee3;
+    }
+
+    public void setDepartmentEmployee4(String departmentEmployee4) {
+
+        this.departmentEmployee4 = departmentEmployee4;
+    }
+
+    public void setDepartmentEmployee5(String departmentEmployee5) {
+
+        this.departmentEmployee5 = departmentEmployee5;
+    }
+
+    public void setSalaryEmployee(int salaryEmployee) {
+
+        this.salaryEmployee = salaryEmployee;
+    }
+
+    public String toString() {
+        return "Id: " + id + " ФИО: " + fullName + " Отдел: " + departmentEmployee1 + " Зарплата: " + salaryEmployee;
+    }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,70 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee ivanovIvanIvanovich = new Employee(0,"Иванов И. И.", "менджмент", 120_000);
+        System.out.println("ID: " + ivanovIvanIvanovich.getId() + ", Сотрудник: " + ivanovIvanIvanovich.getFullName() + ", отдел: " + ivanovIvanIvanovich.getDepartmentEmployee1() + ", зарплата: " + ivanovIvanIvanovich.getSalaryEmployee());
+
+        Employee sidorovaOlgaViktorovna = new Employee(0,"Сидорова О. В.", "секретариат", 78_000);
+        System.out.println("ID: " + sidorovaOlgaViktorovna.getId() + ", Сотрудник: " + sidorovaOlgaViktorovna.getFullName() + ", отдел: " + sidorovaOlgaViktorovna.getDepartmentEmployee5() + ", зарплата: " + sidorovaOlgaViktorovna.getSalaryEmployee());
+
+        Employee sidorovaOlgaViktorovna1 = new Employee(0,"Сидорова О. В.", "секретариат", 57_000);
+        System.out.println("ID: " + sidorovaOlgaViktorovna1.getId() + ", Сотрудник: " + sidorovaOlgaViktorovna1.getFullName() + ", отдел: " + sidorovaOlgaViktorovna1.getDepartmentEmployee5() + ", зарплата: " + sidorovaOlgaViktorovna1.getSalaryEmployee());
+
+        Employee sidorovaOlgaViktorovna2 = new Employee(0,"Сидорова О. В.", "секретариат", 98_000);
+        System.out.println("ID: " + sidorovaOlgaViktorovna2.getId() + ", Сотрудник: " + sidorovaOlgaViktorovna2.getFullName() + ", отдел: " + sidorovaOlgaViktorovna2.getDepartmentEmployee5() + ", зарплата: " + sidorovaOlgaViktorovna2.getSalaryEmployee());
+
+        Employee sidorovaOlgaViktorovna3 = new Employee(0,"Сидорова О. В.", "секретариат", 110_000);
+        System.out.println("ID: " + sidorovaOlgaViktorovna3.getId() + ", Сотрудник: " + sidorovaOlgaViktorovna3.getFullName() + ", отдел: " + sidorovaOlgaViktorovna3.getDepartmentEmployee5() + ", зарплата: " + sidorovaOlgaViktorovna3.getSalaryEmployee());
+
+        Employee sidorovaOlgaViktorovna4 = new Employee(0,"Сидорова О. В.", "секретариат", 69_000);
+        System.out.println("ID: " + sidorovaOlgaViktorovna4.getId() + ", Сотрудник: " + sidorovaOlgaViktorovna4.getFullName() + ", отдел: " + sidorovaOlgaViktorovna4.getDepartmentEmployee5() + ", зарплата: " + sidorovaOlgaViktorovna4.getSalaryEmployee());
+
+        Employee sidorovaOlgaViktorovna5 = new Employee(0,"Сидорова О. В.", "секретариат", 87_000);
+        System.out.println("ID: " + sidorovaOlgaViktorovna5.getId() + ", Сотрудник: " + sidorovaOlgaViktorovna5.getFullName() + ", отдел: " + sidorovaOlgaViktorovna5.getDepartmentEmployee5() + ", зарплата: " + sidorovaOlgaViktorovna5.getSalaryEmployee());
+
+        Employee sidorovaOlgaViktorovna6 = new Employee(0,"Сидорова О. В.", "секретариат", 89_000);
+        System.out.println("ID: " + sidorovaOlgaViktorovna6.getId() + ", Сотрудник: " + sidorovaOlgaViktorovna6.getFullName() + ", отдел: " + sidorovaOlgaViktorovna6.getDepartmentEmployee5() + ", зарплата: " + sidorovaOlgaViktorovna6.getSalaryEmployee());
+
+        Employee sidorovaOlgaViktorovna7 = new Employee(0,"Сидорова О. В.", "секретариат", 79_000);
+        System.out.println("ID: " + sidorovaOlgaViktorovna7.getId() + ", Сотрудник: " + sidorovaOlgaViktorovna7.getFullName() + ", отдел: " + sidorovaOlgaViktorovna7.getDepartmentEmployee5() + ", зарплата: " + sidorovaOlgaViktorovna7.getSalaryEmployee());
+
+        Employee sidorovaOlgaViktorovna8 = new Employee(0,"Сидорова О. В.", "секретариат", 77_000);
+        System.out.println("ID: " + sidorovaOlgaViktorovna8.getId() + ", Сотрудник: " + sidorovaOlgaViktorovna8.getFullName() + ", отдел: " + sidorovaOlgaViktorovna8.getDepartmentEmployee5() + ", зарплата: " + sidorovaOlgaViktorovna8.getSalaryEmployee());
+
+        System.out.println("==============================================================================");
+
+        Employee[] employees = new Employee[10];
+        employees[0] = new Employee(0,"Иванов И. И., " , "менджмент, ", 120_000);
+        employees[1] = new Employee(0,"Сидорова О. В., ", "секретариат, ", 78_000);
+        employees[2] = new Employee(0,"Сидорова О. В., ", "секретариат, ", 57_000);
+        employees[3] = new Employee(0,"Сидорова О. В., ", "секретариат, ", 98_000);
+        employees[4] = new Employee(0,"Сидорова О. В., ", "секретариат, ", 110_000);
+        employees[5] = new Employee(0,"Сидорова О. В., ", "секретариат, ", 69_000);
+        employees[6] = new Employee(0,"Сидорова О. В., ", "секретариат, ", 87_000);
+        employees[7] = new Employee(0,"Сидорова О. В., ", "секретариат, ", 89_000);
+        employees[8] = new Employee(0,"Сидорова О. В., ", "секретариат, ", 79_000);
+        employees[9] = new Employee(0,"Сидорова О. В., ", "секретариат, ", 77_000);
+
+        for (Employee i: employees) {
+            System.out.println(i);
+        }
+
+
+        int result = (SalaryEmployee);
+
+
+
     }
+        public static int findEmployeeWithMinSalary(int SalaryEmployee) {
+        Employee[] employees = new Employee[10];
+
+        int maxSalaryEmployee = -1;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] > maxSalaryEmployee) {
+                maxSalaryEmployee = employees[i];
+            }
+        }
+        return SalaryEmployee;
+
+}
 }
